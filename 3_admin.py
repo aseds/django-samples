b2a4e115a151c0b3cb8dd4cfa3dc936d179b15d8
aseds <s.sami.ghayeni@gmail.com>
2015-01-13 20:11:47 -0800

    Update 3_admin.py

--- 3_admin.py
+++ 3_admin.py
@@ -39,3 +39,6 @@ admin.site.register(Question, QuestionAdmin)
 
 //-------------------------------------------------//
 
+// You’ll follow this pattern – create a model admin object,
+// then pass it as the second argument to admin.site.register() – any time you
+// need to change the admin options for an object.
